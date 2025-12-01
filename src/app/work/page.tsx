00a3cbd33f693de77f60a0b5d90b708cde4ba706
poylib <poy_lib@naver.com>
2025-12-01 14:19:35 +0900

    fix: build error

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,5 +1,6 @@
 import WorkList from "@/components/work/WorkList";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
     title: "Work | Socon",
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
 export default function WorkPage() {
     return (
         <main className="min-h-screen bg-neutral-950 text-white selection:bg-white selection:text-black">
-            <WorkList />
+            <Suspense>
+                <WorkList />
+            </Suspense>
         </main>
     );
 }
